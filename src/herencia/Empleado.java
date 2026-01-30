44293a8f3ceca34ec291301e7f0980209d6f4d25
Jonathan Canales <jc1stson@gmail.com>
2026-01-30 09:49:54 -0600

    Commit 1

--- src/herencia/Empleado.java
+++ src/herencia/Empleado.java
@@ -5,10 +5,41 @@
  */
 package herencia;
 
+import java.time.LocalDate;
+        
 /**
  *
  * @author Fabio Sierra
  */
 public class Empleado {
+    protected String codigo;
+    protected String nombreEmpleado;
+    protected LocalDate fechaContratacion;
+    protected double salarioBase;
+    protected int horasTrabajadas;
+
+    public Empleado(String codigo, String nombreEmpleado, double salarioBase, int horasTrabajadas) {
+        this.codigo = codigo;
+        this.nombreEmpleado = nombreEmpleado;
+        this.salarioBase = salarioBase;
+        this.horasTrabajadas = horasTrabajadas;
+        this.fechaContratacion = LocalDate.now();
+        this.horasTrabajadas=0;
+    }
     
+    public void registrarHorasTrabajadas(int horas){
+        if(horas>0){
+            this.horasTrabajadas += horas;
+        }
+    }
+    
+    public double calcularPago(){
+        double pagoPrincipal = (salarioBase/160)*horasTrabajadas;
+        double deduccion = pagoPrincipal*0.035;
+        return pagoPrincipal-deduccion;
+    }
+    
+    public String mostrarInformacion(){
+         return String.format("ID: "+codigo+ " Nombre: "+nombreEmpleado+" Fecha de Contratacion: "+fechaContratacion.toString());
+    }
 }
