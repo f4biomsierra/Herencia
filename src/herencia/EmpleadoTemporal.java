9c73132fdfd83fe8dc2f599c7df12e41bf79911c
f4biomsierra <fabiosierra200623@gmail.com>
2026-01-30 10:12:11 -0600

    Commit de subclases terminadas

--- src/herencia/EmpleadoTemporal.java
+++ src/herencia/EmpleadoTemporal.java
@@ -10,15 +10,18 @@ import java.time.LocalDate;
 public class EmpleadoTemporal extends Empleado {
     protected LocalDate finContratacion;
     
-    public EmpleadoTemporal(LocalDate finContratacion){
+    public EmpleadoTemporal(String codigo, String nombreEmpleado, double salarioBase, int horasTrabajadas, LocalDate finContratacion){
+        super(codigo, nombreEmpleado, salarioBase, horasTrabajadas);
         this.finContratacion=finContratacion;
     }
     
     public double pagoCondicionado(){
         LocalDate fechaActual=LocalDate.now();
         if(fechaActual.isBefore(finContratacion) || fechaActual.isEqual(finContratacion)){
-            
-        }
+            return (this.horasTrabajadas/160)*this.salarioBase;
+        } else{
+            return 0.0;
+        } 
     }
     
     public void actualizarContratacion(LocalDate fechaNueva){
@@ -26,6 +29,6 @@ public class EmpleadoTemporal extends Empleado {
     }
     
     public String mostrarInformacion(){
-        
+        return super.mostrarInformacion()+", Fecha de Fin de Contrato: "+finContratacion;
     }
 }
--- src/herencia/EmpleadoVentas.java
+++ src/herencia/EmpleadoVentas.java
@@ -11,7 +11,8 @@ public class EmpleadoVentas extends Empleado {
     protected double []ventasMensuales;
     protected double tasaComision;
     
-    public EmpleadoVentas(double []ventasMensuales, double tasaComision){
+    public EmpleadoVentas(String codigo, String nombreEmpleado, double salarioBase, int horasTrabajadas, double []ventasMensuales, double tasaComision){
+        super(codigo, nombreEmpleado, salarioBase, horasTrabajadas);
         this.ventasMensuales=new double[12];
         this.tasaComision=tasaComision;
     }
@@ -23,17 +24,24 @@ public class EmpleadoVentas extends Empleado {
     
     public double calculoComision(){
         int mesActual=LocalDate.now().getMonthValue()-1;
+        return this.ventasMensuales[mesActual]*this.tasaComision;
     }
     
-    public double pagoMensual(){
-        
+    public double calcularPago(){
+        double proporcional=(this.horasTrabajadas/160)*this.salarioBase;
+        double comision=calculoComision();
+        return proporcional+comision;
     }
     
     public double ventasAnuales(){
-        
+        double total=0;
+        for(int contador=0;contador<ventasMensuales.length;contador++){
+            total=total+ventasMensuales[contador];
+        }
+        return total;
     }
     
     public String mostrarInformacion(){
-        
+        return super.mostrarInformacion()+", Ventas Anuales: "+String.format("%.2f", ventasAnuales());
     }
 }
